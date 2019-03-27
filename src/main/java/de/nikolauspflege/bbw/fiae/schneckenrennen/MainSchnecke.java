f77a0cd8e56e76f41a43b87f14851aa57f5249d8
whaenel <vonhenninghausen@web.de>
2019-03-27 19:28:44 +0100

    init testrebase

--- src/main/java/de/nikolauspflege/bbw/fiae/schneckenrennen/MainSchnecke.java
+++ src/main/java/de/nikolauspflege/bbw/fiae/schneckenrennen/MainSchnecke.java
@@ -5,6 +5,7 @@ public class MainSchnecke {
 	public static void main(String[] args) {
 		
 		Rennschnecke r1 = new Rennschnecke("Gary", "Speedy", 100);
+		Rennschnecke r2 = new Rennschnecke("Gary", "Speedy2", 100);
 		System.out.println(r1.toString());
 		r1.krieche();
 		System.out.println(r1.toString());
